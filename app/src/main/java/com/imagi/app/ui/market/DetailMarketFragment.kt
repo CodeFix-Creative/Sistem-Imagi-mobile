e7de862cdcb74d47ab6c5e12a08dc1501c85235c
angga pande <anggapande21@gmail.com>
2021-05-30 13:18:59 +0800

    fix map

--- app/src/main/java/com/imagi/app/ui/market/DetailMarketFragment.kt
+++ app/src/main/java/com/imagi/app/ui/market/DetailMarketFragment.kt
@@ -1,16 +1,19 @@
 package com.imagi.app.ui.market
 
 import android.annotation.SuppressLint
+import android.content.Intent
 import android.os.Bundle
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import android.widget.Button
 import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.fragment.app.Fragment
 import androidx.lifecycle.ViewModelProvider
 import androidx.recyclerview.widget.GridLayoutManager
 import androidx.recyclerview.widget.RecyclerView
+import com.imagi.app.MapsActivity
 import com.imagi.app.R
 import com.imagi.app.adapter.MarketAdapter
 import com.imagi.app.network.Market
@@ -19,6 +22,8 @@ import kotlinx.android.synthetic.main.detail_market_fragment.*
 class DetailMarketFragment : Fragment() {
 
     private var enableScrolling = true
+
+    lateinit var buttonLocation : Button
 
     var markets = ArrayList<Market>()
 
@@ -42,11 +47,18 @@ class DetailMarketFragment : Fragment() {
             inflater: LayoutInflater, container: ViewGroup?,
             savedInstanceState: Bundle?
     ): View {
+        val myInflatedView: View = inflater.inflate(R.layout.detail_market_fragment, container, false)
         (activity as AppCompatActivity).supportActionBar?.title = "Detail Produk"
         (activity as AppCompatActivity).supportActionBar?.setDisplayHomeAsUpEnabled(true)
         (activity as AppCompatActivity).supportActionBar?.setDisplayShowHomeEnabled(true)
 
-        return inflater.inflate(R.layout.detail_market_fragment, container, false)
+        buttonLocation = myInflatedView.findViewById(R.id.buttonLocation)
+        buttonLocation.setOnClickListener {
+            val intent = Intent(myInflatedView.context, MapsActivity::class.java)
+            startActivity(intent)
+        }
+
+        return myInflatedView
     }
 
     @SuppressLint("ClickableViewAccessibility")
